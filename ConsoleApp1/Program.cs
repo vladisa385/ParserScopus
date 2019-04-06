69df3bee553a1b25bf05e821c59ba312ad57b379
vladisa385 <vladisa375@gmail.com>
2019-04-06 12:57:12 +0700

    123

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -17,19 +17,23 @@ namespace ParserScopus
             List<ResultEmail> emailWithAuthor = new List<ResultEmail>();
             var firstUrl = "https://www.scopus.com/record/display.uri?origin=recordpage&eid=2-s2.0-85063440381&citeCnt=0&noHighlight=false&sort=plf-f&src=s&nlo=&nlr=&nls=&sid=05b7b965a025ff8795f30fbd1fa9dcef&sot=b&sdt=cl&cluster=scopubyr%2c%222019%22%2ct&sl=110&s=AF-ID%28%22Tomskij+Gosudarstvennyj+Universitet+Sistem+Upravlenija+i+Radioelektroniki%22+60010892%29+AND+SUBJAREA%28PHYS%29&relpos=0";
             string nextUrl = firstUrl;
+            var countArticles = 42;
+            var currentArticle = 1;
             do
             {
-
                 var result = parser.ParseSpecificArticle(nextUrl, out nextUrl);
+                //if (countArticles == 0)
+                //    countArticles = parser.GetCountArticle(firstUrl);
+                Console.WriteLine($"CurrentArticle {currentArticle} from {countArticles}");
                 if (result.Count != 0)
                     emailWithAuthor.AddRange(result);
-                foreach (ResultEmail var in result)
-                {
-                    Console.WriteLine($"{var.Fio}!!!{var.Email}");
-                }
-
+                currentArticle += 1;
             } while (nextUrl != null);
 
+            foreach (var item in emailWithAuthor)
+            {
+                Console.WriteLine($"{item.Email} {item.Fio}");
+            }
             Console.ReadKey();
         }
     }
