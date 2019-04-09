a6ae0109597bca1cbbabdce8c10e90dd94f273e2
vladisa385 <vladisa375@gmail.com>
2019-04-09 20:34:42 +0700

    привет

---
+++ ViewScopusParser/ScopusParserWinForms.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace ViewScopusParser
+{
+    partial class ScopusParserWinForms
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ScopusParserWinForms));
+            this.ReturnedEmailDataGrid = new System.Windows.Forms.DataGridView();
+            this.FIOColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.EmailColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.URLTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.StartParseButton = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.CurrentPagelabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.MaxPagelabel = new System.Windows.Forms.Label();
+            this.CountPagesLabel = new System.Windows.Forms.Label();
+            this.PagesCounTextBox = new System.Windows.Forms.TextBox();
+            this.ExportExcelButton = new System.Windows.Forms.Button();
+            this.ProgressGroupBox = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ReturnedEmailDataGrid)).BeginInit();
+            this.ProgressGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ReturnedEmailDataGrid
+            // 
+            this.ReturnedEmailDataGrid.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ReturnedEmailDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ReturnedEmailDataGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.FIOColumn,
+            this.EmailColumn});
+            this.ReturnedEmailDataGrid.Location = new System.Drawing.Point(12, 87);
+            this.ReturnedEmailDataGrid.Name = "ReturnedEmailDataGrid";
+            this.ReturnedEmailDataGrid.ReadOnly = true;
+            this.ReturnedEmailDataGrid.Size = new System.Drawing.Size(426, 191);
+            this.ReturnedEmailDataGrid.TabIndex = 0;
+            // 
+            // FIOColumn
+            // 
+            this.FIOColumn.HeaderText = "Фамилия\\Имя";
+            this.FIOColumn.Name = "FIOColumn";
+            this.FIOColumn.ReadOnly = true;
+            this.FIOColumn.Width = 180;
+            // 
+            // EmailColumn
+            // 
+            this.EmailColumn.HeaderText = "Email";
+            this.EmailColumn.Name = "EmailColumn";
+            this.EmailColumn.ReadOnly = true;
+            this.EmailColumn.Width = 180;
+            // 
+            // URLTextBox
+            // 
+            this.URLTextBox.Location = new System.Drawing.Point(43, 22);
+            this.URLTextBox.Name = "URLTextBox";
+            this.URLTextBox.Size = new System.Drawing.Size(270, 20);
+            this.URLTextBox.TabIndex = 1;
+            this.URLTextBox.Text = resources.GetString("URLTextBox.Text");
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "URL";
+            // 
+            // StartParseButton
+            // 
+            this.StartParseButton.Location = new System.Drawing.Point(363, 19);
+            this.StartParseButton.Name = "StartParseButton";
+            this.StartParseButton.Size = new System.Drawing.Size(75, 23);
+            this.StartParseButton.TabIndex = 3;
+            this.StartParseButton.Text = "Начать";
+            this.StartParseButton.UseVisualStyleBackColor = true;
+            this.StartParseButton.Click += new System.EventHandler(this.StartParseButton_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(6, 37);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(426, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // CurrentPagelabel
+            // 
+            this.CurrentPagelabel.AutoSize = true;
+            this.CurrentPagelabel.Location = new System.Drawing.Point(6, 18);
+            this.CurrentPagelabel.Name = "CurrentPagelabel";
+            this.CurrentPagelabel.Size = new System.Drawing.Size(13, 13);
+            this.CurrentPagelabel.TabIndex = 5;
+            this.CurrentPagelabel.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "из";
+            // 
+            // MaxPagelabel
+            // 
+            this.MaxPagelabel.AutoSize = true;
+            this.MaxPagelabel.Location = new System.Drawing.Point(50, 18);
+            this.MaxPagelabel.Name = "MaxPagelabel";
+            this.MaxPagelabel.Size = new System.Drawing.Size(13, 13);
+            this.MaxPagelabel.TabIndex = 7;
+            this.MaxPagelabel.Text = "0";
+            // 
+            // CountPagesLabel
+            // 
+            this.CountPagesLabel.AutoSize = true;
+            this.CountPagesLabel.Location = new System.Drawing.Point(12, 51);
+            this.CountPagesLabel.Name = "CountPagesLabel";
+            this.CountPagesLabel.Size = new System.Drawing.Size(135, 13);
+            this.CountPagesLabel.TabIndex = 8;
+            this.CountPagesLabel.Text = "Кол-во статей для парса:";
+            // 
+            // PagesCounTextBox
+            // 
+            this.PagesCounTextBox.Location = new System.Drawing.Point(150, 48);
+            this.PagesCounTextBox.Name = "PagesCounTextBox";
+            this.PagesCounTextBox.Size = new System.Drawing.Size(49, 20);
+            this.PagesCounTextBox.TabIndex = 9;
+            this.PagesCounTextBox.Text = "1";
+            this.PagesCounTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.PagesCounTextBox_KeyPress);
+            // 
+            // ExportExcelButton
+            // 
+            this.ExportExcelButton.Location = new System.Drawing.Point(337, 51);
+            this.ExportExcelButton.Name = "ExportExcelButton";
+            this.ExportExcelButton.Size = new System.Drawing.Size(101, 23);
+            this.ExportExcelButton.TabIndex = 10;
+            this.ExportExcelButton.Text = "Экспорт в Excel";
+            this.ExportExcelButton.UseVisualStyleBackColor = true;
+            this.ExportExcelButton.Click += new System.EventHandler(this.ExportExcelbutton_Click);
+            // 
+            // ProgressGroupBox
+            // 
+            this.ProgressGroupBox.Controls.Add(this.progressBar1);
+            this.ProgressGroupBox.Controls.Add(this.CurrentPagelabel);
+            this.ProgressGroupBox.Controls.Add(this.label3);
+            this.ProgressGroupBox.Controls.Add(this.MaxPagelabel);
+            this.ProgressGroupBox.Location = new System.Drawing.Point(12, 277);
+            this.ProgressGroupBox.Name = "ProgressGroupBox";
+            this.ProgressGroupBox.Size = new System.Drawing.Size(425, 66);
+            this.ProgressGroupBox.TabIndex = 11;
+            this.ProgressGroupBox.TabStop = false;
+            this.ProgressGroupBox.Visible = false;
+            // 
+            // ScopusParserWinForms
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 355);
+            this.Controls.Add(this.ProgressGroupBox);
+            this.Controls.Add(this.ExportExcelButton);
+            this.Controls.Add(this.PagesCounTextBox);
+            this.Controls.Add(this.CountPagesLabel);
+            this.Controls.Add(this.StartParseButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.URLTextBox);
+            this.Controls.Add(this.ReturnedEmailDataGrid);
+            this.Name = "ScopusParserWinForms";
+            this.Text = "Scopus Parser";
+            ((System.ComponentModel.ISupportInitialize)(this.ReturnedEmailDataGrid)).EndInit();
+            this.ProgressGroupBox.ResumeLayout(false);
+            this.ProgressGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView ReturnedEmailDataGrid;
+        private System.Windows.Forms.TextBox URLTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button StartParseButton;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label CurrentPagelabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label MaxPagelabel;
+        private System.Windows.Forms.Label CountPagesLabel;
+        private System.Windows.Forms.TextBox PagesCounTextBox;
+        private System.Windows.Forms.Button ExportExcelButton;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FIOColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn EmailColumn;
+        private System.Windows.Forms.GroupBox ProgressGroupBox;
+    }
+}
+
---
+++ ViewScopusParser/ScopusParserWinForms.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using ScopusModel;
+using Microsoft.Office.Interop.Excel;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using Label = System.Windows.Forms.Label;
+using TextBox = System.Windows.Forms.TextBox;
+
+namespace ViewScopusParser
+{
+    public partial class ScopusParserWinForms : Form
+    {
+        public ScopusParserWinForms()
+        {
+            InitializeComponent();
+        }
+
+        private void ParsePageFromUrlTextBox(IParse parser)
+        {
+
+            string firstUrl = URLTextBox.Text;
+            var nextUrl = firstUrl;
+            var countArticles = Convert.ToInt32(PagesCounTextBox.Text);
+
+            progressBar1.Invoke((MethodInvoker)(() => progressBar1.Maximum = countArticles));
+            progressBar1.Invoke((MethodInvoker)(() => progressBar1.Value = 0));
+            MaxPagelabel.Invoke((MethodInvoker)(() => MaxPagelabel.Text = countArticles.ToString()));
+            for (var currentArticle = 1; currentArticle <= countArticles && nextUrl != null; currentArticle++)
+            {
+                var result = parser.ParseSpecificArticle(nextUrl);
+
+                if (countArticles > parser.GetCountArticle(firstUrl))
+                {
+                    countArticles = parser.GetCountArticle(firstUrl);
+                    progressBar1.Invoke((MethodInvoker)(() => progressBar1.Maximum = countArticles));
+                }
+
+                foreach (var item in result)
+                {
+                    ReturnedEmailDataGrid.Invoke((MethodInvoker)(() => ReturnedEmailDataGrid.Rows.Add(item.Fio, item.Email)));
+                }
+                nextUrl = parser.GetNextArticle(nextUrl);
+                CurrentPagelabel.Invoke((MethodInvoker)(() => CurrentPagelabel.Text = currentArticle.ToString()));
+                progressBar1.Invoke((MethodInvoker)(() => progressBar1.Value = currentArticle));
+            }
+
+
+        }
+
+        private void StartParseButtonClickAsync()
+        {
+
+            var driver = CreateIWebDriverFabricMethod();
+            IParse parser = new ScopusParser(driver);
+            try
+            {
+                ParsePageFromUrlTextBox(parser);
+            }
+
+            catch (NoSuchElementException)
+            {
+                MessageBox.Show(@"Проверьте Ваше интернет-соединение. Возможно, вы забыли подключить VPN или указали неверную ссылку.",
+                    "Ошибка соединения!", buttons: MessageBoxButtons.OK, icon: MessageBoxIcon.Warning);
+
+            }
+            catch (WebDriverException)
+            {
+                MessageBox.Show(@"При запуске парсера возникла проблема. Вы указали неверный URL.",
+                    @"Ошибка соединения!", buttons: MessageBoxButtons.OK, icon: MessageBoxIcon.Warning);
+            }
+            //catch (Exception ee)
+            //{
+            //    MessageBox.Show(@"При запуске парсера возникла проблема. Попробуйте заново запустить программу.",
+            //        @"Ошибка запуска!", buttons: MessageBoxButtons.OK, icon: MessageBoxIcon.Warning);
+            //}
+            finally
+            {
+                parser.Dispose();
+                ProgressGroupBox.Invoke((MethodInvoker)(() => ProgressGroupBox.Visible = false));
+                StartParseButton.Invoke((MethodInvoker)(() => StartParseButton.Enabled = true));
+            }
+
+        }
+
+        private void ExportExcelbutton_Click(object sender, EventArgs e)
+        {
+            Microsoft.Office.Interop.Excel.Application excelapp = new Microsoft.Office.Interop.Excel.Application();
+            excelapp.AlertBeforeOverwriting = false;
+            excelapp.DisplayAlerts = false;
+
+            SaveFileDialog sfd = new SaveFileDialog()
+            {
+                Filter = "MS Excel documents (*.xlsx)|*.xlsx",
+                DefaultExt = "*.xlsx",
+                FileName = "1",
+                Title = "Укажите директорию и имя файла для сохранения"
+            };
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    Workbook workbook = excelapp.Workbooks.Add();
+                    Worksheet worksheet = workbook.ActiveSheet;
+
+                    for (int i = 1; i < ReturnedEmailDataGrid.RowCount + 1; i++)
+                    {
+                        for (int j = 1; j < ReturnedEmailDataGrid.ColumnCount + 1; j++)
+                        {
+                            worksheet.Rows[i].Columns[j] = ReturnedEmailDataGrid.Rows[i - 1].Cells[j - 1].Value;
+                        }
+                    }
+
+
+                    workbook.SaveAs(sfd.FileName);
+                    excelapp.Quit();
+                }
+                catch (Exception)
+                {
+                    //return;
+                    excelapp.Quit();
+                }
+            }
+
+        }
+
+        private void PagesCounTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+            e.KeyChar != '.')
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            if (e.KeyChar == '.' && ((TextBox)sender).Text.IndexOf('.') > -1)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private IWebDriver CreateIWebDriverFabricMethod()
+        {
+            IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            return driver;
+        }
+
+        private void StartParseButton_Click(object sender, EventArgs e)
+        {
+            ProgressGroupBox.Visible = true;
+            StartParseButton.Enabled = false;
+            ReturnedEmailDataGrid.Rows.Clear();
+            Thread thread = new Thread(StartParseButtonClickAsync);
+            thread.Start();
+        }
+    }
+}
