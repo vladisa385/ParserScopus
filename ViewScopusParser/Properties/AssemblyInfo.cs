4a4dca6b8443d93dae8678e6cdd0f086dfa49414
vladisa385 <vladisa375@gmail.com>
2019-04-12 23:01:27 +0700

    Добавил в описание себя и поставил зависимость на .netfraemwork пониже

--- ViewScopusParser/Properties/AssemblyInfo.cs
+++ ViewScopusParser/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанных со сборкой.
 [assembly: AssemblyTitle("ViewScopusParser")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("По любым вопросам vladisa375@gmail.com")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ViewScopusParser")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Vladislav Konyukov,Kiril Yashchenkov © 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
